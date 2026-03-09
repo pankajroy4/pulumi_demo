a631e2c4ed994789f99d025f2a80c53d488abeac
pankajroy4 <pankajroy842002@gmail.com>
2026-03-09 16:44:39 +0530

    WIP-new-2

--- infra/components/api.ts
+++ infra/components/api.ts
@@ -1,17 +1,69 @@
 // infra/components/api.ts
+// import * as web from "@pulumi/azure-native/web";
+
+// export function createApi(
+//     name: string,
+//     rg: any,
+//     location: string,
+//     dbConnection: any,
+//     keyVaultUri: any
+// ) {
+
+//     const plan = new web.AppServicePlan(`${name}-plan`, {
+//         resourceGroupName: rg,
+//         location,
+//         sku: {
+//             name: "B1",
+//             tier: "Basic",
+//         },
+//         kind: "Linux",
+//         reserved: true,
+//     });
+
+//     const app = new web.WebApp(`${name}-api`, {
+//         resourceGroupName: rg,
+//         location,
+//         serverFarmId: plan.id,
+//         siteConfig: {
+//             linuxFxVersion: "DOCKER|docker.io/pankajroy4/fastapi-backend:latest",
+//             alwaysOn: true,
+//             appSettings: [
+//                 { name: "DB_HOST", value: dbConnection},
+//                 { name: "DB_USER", value: "pgadmin"},
+//                 { name: "DB_PASSWORD",value: "StrongPassword123!"},
+//                 { name: "DB_NAME",value: "appdb"},
+//                 { name: "DB_PORT", value: "5432"},
+//                 { name: "JWT_SIGNING_KEY", value: "dummy" },
+//                 { name: "KEYVAULT_URI", value: keyVaultUri },
+//                 { name: "WEBSITES_PORT", value: "8000"}
+//             ],
+//         },
+//     });
+
+//     return {
+//         apiUrl: app.defaultHostName.apply(
+//             h => `https://${h}`
+//         ),
+//     };
+// }
+
+
+
+
 import * as web from "@pulumi/azure-native/web";
+import * as pulumi from "@pulumi/pulumi";
 
 export function createApi(
     name: string,
-    rg: any,
+    rg: pulumi.Input<string>,
     location: string,
-    dbConnection: any,
-    keyVaultUri: any
+    postgres: any,
+    keyVaultUri: pulumi.Input<string>
 ) {
 
     const plan = new web.AppServicePlan(`${name}-plan`, {
         resourceGroupName: rg,
-        location,
+        location: location,
         sku: {
             name: "B1",
             tier: "Basic",
@@ -22,51 +74,28 @@ export function createApi(
 
     const app = new web.WebApp(`${name}-api`, {
         resourceGroupName: rg,
-        location,
+        location: location,
         serverFarmId: plan.id,
         siteConfig: {
             linuxFxVersion: "DOCKER|docker.io/pankajroy4/fastapi-backend:latest",
             alwaysOn: true,
             appSettings: [
-                {
-                    name: "DB_HOST",
-                    value: dbConnection,
-                },
-                {
-                    name: "DB_USER",
-                    value: "pgadmin",
-                },
-                {
-                    name: "DB_PASSWORD",
-                    value: "StrongPassword123!",
-                },
-                {
-                    name: "DB_NAME",
-                    value: "appdb",
-                },
-                {
-                    name: "DB_PORT",
-                    value: "5432",
-                },
-                {
-                    name: "JWT_SIGNING_KEY",
-                    value: "dummy",
-                },
-                {
-                    name: "KEYVAULT_URI",
-                    value: keyVaultUri,
-                },
-                {
-                    name: "WEBSITES_PORT",
-                    value: "8000",
-                }
+                { name: "DB_HOST",value: postgres.host},
+                { name: "DB_USER", value: "pgadmin"},
+                { name: "DB_PASSWORD", value: "StrongPassword123!"},
+                { name: "DB_NAME", value: "appdb"},
+                { name: "DB_PORT", value: "5432"},
+                { name: "JWT_SIGNING_KEY", value: "dummy"},
+                { name: "KEYVAULT_URI",value: keyVaultUri},
+                { name: "WEBSITES_PORT", value: "8000"},
+                { name: "ENVIRONMENT", value: "production"}
             ],
         },
     });
 
     return {
         apiUrl: app.defaultHostName.apply(
-            h => `https://${h}`
+            hostname => `https://${hostname}`
         ),
     };
 }
