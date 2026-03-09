dd48f649b6f26fa7064548b4e296ea43c80ca674
pankajroy4 <pankajroy842002@gmail.com>
2026-03-09 16:19:25 +0530

    WIP-new-2

--- infra/components/postgres.ts
+++ infra/components/postgres.ts
@@ -1,5 +1,59 @@
 // infra/components/postgres.ts
 import * as postgres from "@pulumi/azure-native/dbforpostgresql";
+import * as pulumi from "@pulumi/pulumi";
+
+// export function createPostgres(
+//     name: string,
+//     rg: any,
+//     location: string,
+//     subnetId: any
+// ) {
+
+
+
+//     const server = new postgres.Server(`${name}-pg`, {
+//         resourceGroupName: rg,
+//         location,
+//         version: "14",
+//         administratorLogin: "pgadmin",
+//         administratorLoginPassword: "StrongPassword123!",
+//         storage: {
+//             storageSizeGB: 32,
+//         },
+//         sku: {
+//             name: "Standard_B1ms",
+//             tier: "Burstable",
+//         },
+//     });
+
+
+//     const firewall = new postgres.FirewallRule(`${name}-allow-azure`, {
+//         resourceGroupName: rg,
+//         serverName: server.name,
+//         startIpAddress: "0.0.0.0",
+//         endIpAddress: "0.0.0.0",
+//     });
+
+
+//     const db = new postgres.Database(`${name}-db`, {
+//         resourceGroupName: rg,
+//         serverName: server.name,
+//         databaseName: "appdb",
+//     });
+
+//     // const connectionString = server.fullyQualifiedDomainName.apply(
+//     //     host =>
+//     //         `postgresql://pgadmin:StrongPassword123!@${host}:5432/appdb`
+//     // );
+
+//     const connectionString = pulumi.interpolate`postgresql://${adminUser}:${adminPassword}@${server.fullyQualifiedDomainName}:5432/${databaseName}?sslmode=require`;
+
+//     return {
+//         host: server.fullyQualifiedDomainName,
+//         connectionString
+//     };
+// }
+
 
 export function createPostgres(
     name: string,
@@ -8,14 +62,16 @@ export function createPostgres(
     subnetId: any
 ) {
 
-
+    const adminUser = "pgadmin";
+    const adminPassword = "StrongPassword123!";
+    const databaseName = "appdb";
 
     const server = new postgres.Server(`${name}-pg`, {
         resourceGroupName: rg,
         location,
         version: "14",
-        administratorLogin: "pgadmin",
-        administratorLoginPassword: "StrongPassword123!",
+        administratorLogin: adminUser,
+        administratorLoginPassword: adminPassword,
         storage: {
             storageSizeGB: 32,
         },
@@ -25,7 +81,6 @@ export function createPostgres(
         },
     });
 
-
     const firewall = new postgres.FirewallRule(`${name}-allow-azure`, {
         resourceGroupName: rg,
         serverName: server.name,
@@ -33,17 +88,15 @@ export function createPostgres(
         endIpAddress: "0.0.0.0",
     });
 
-
     const db = new postgres.Database(`${name}-db`, {
         resourceGroupName: rg,
         serverName: server.name,
-        databaseName: "appdb",
+        databaseName,
     });
 
-    const connectionString = server.fullyQualifiedDomainName.apply(
-        host =>
-            `postgresql://pgadmin:StrongPassword123!@${host}:5432/appdb`
-    );
+    const connectionString = pulumi.interpolate`
+postgresql://${adminUser}@${server.name}:${adminPassword}@${server.fullyQualifiedDomainName}:5432/${databaseName}?sslmode=require
+`;
 
     return {
         host: server.fullyQualifiedDomainName,
